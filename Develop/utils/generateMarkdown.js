f1da30d20e0a4e7c0e39cdf01eb0e9e6f6614674
Finian Corbett <finian97@gmail.com>
2024-05-09 21:45:08 -0400

    Created function for rendering a license badge, link, and section based on license tyoe

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+// Function to render a license badge, link, and section based on the license type
+function renderLicense(license) {
+  if (!license) return { badge: '', link: '', section: '' };
+  const badge = `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
+  const link = `[License](https://opensource.org/licenses/${license})`;
+  const section = `## License
+
+This project is licensed under the ${license} license. See the [${license} license](${link}) for more details.
+`;
+  return { badge, link, section };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
