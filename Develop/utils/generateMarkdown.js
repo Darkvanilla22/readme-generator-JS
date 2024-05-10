fcf2216aebac5cc64c2c0f5b3300ee8b8215f256
Finian Corbett <finian97@gmail.com>
2024-05-09 21:47:40 -0400

    Created function to generate markdown for README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,18 +13,50 @@ This project is licensed under the ${license} license. See the [${license} licen
   return { badge, link, section };
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
+  const { badge, section } = renderLicense(data.license);
   return `# ${data.title}
 
+${badge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${section}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For questions about this project, please visit my [GitHub profile](https://github.com/${data.github}), or feel free to [email me](mailto:${data.email}) with any additional questions.
 `;
 }
 
