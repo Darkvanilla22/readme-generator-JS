e92a3082597bc46ce839660970e747398d4b9c9a
Finian Corbett <finian97@gmail.com>
2024-05-09 21:35:03 -0400

    Added several questions to the array

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,19 @@ const fs = require('fs');
 const generateMarkdown = require('./assets/js/generateMD.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+// Array of questions for user input
+const questions = [
+    { type: 'input', name: 'title', message: 'What is the title of your project?' },
+    { type: 'input', name: 'description', message: 'Please provide a description of your project:' },
+    { type: 'input', name: 'installation', message: 'Please provide installation instructions:' },
+    { type: 'input', name: 'usage', message: 'Please provide usage information:' },
+    { type: 'list', name: 'license', message: 'Choose a license for your project:', choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'] },
+    { type: 'input', name: 'contributing', message: 'Please provide contribution guidelines:' },
+    { type: 'input', name: 'tests', message: 'Please provide test instructions:' },
+    { type: 'input', name: 'github', message: 'What is your GitHub username?' },
+    { type: 'input', name: 'email', message: 'What is your email address?' }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
